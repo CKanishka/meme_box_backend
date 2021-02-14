4c667dec54d9997043d301cac1a5b92d5334a66f
Kanishka Chowdhury <ckanishka24@gmail.com>
2021-02-14 16:30:45 +0530

    SwaggerUI added

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const items = require("./routes/api"); //importing the route file
 
-const mongoURI = process.env.MONGODB_URI  // mongoDB URI
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-const port = process.env.PORT || 5000; // port where the server listens
+const mongoURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017" // mongoDB URI
+
+const port = process.env.PORT || 8081; // port where the server listens
 
 const app = express(); // initializing express
 app.use(cors());      // applying cors middeware
 app.use(bodyParser.json()); // applying bodyparser middleware 
 app.use("/", items); // specifying the routes
 
+app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // connecting to mongo
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
